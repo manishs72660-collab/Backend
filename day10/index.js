0d09be81b322c2febac2eb1861ed5c07dcc55665
manish kumar <manishs72660@gmail.com>
2026-04-01 20:00:36 +0530

    momongoose working

---
+++ day10/index.js
@@ -0,0 +1,30 @@
+// require("node:dns/promises").setServers(["1.1.1.1", "8.8.8.8"]);
+// const mongoose = require('mongoose');
+// const { Schema } = mongoose;
+// main().catch(err => console.log(err));
+
+
+
+// async function main() {
+// await mongoose.connect('mongodb+srv://manishsingh:8294068776@codingadda.bzyu9qj.mongodb.net/Bookstore');
+// // const userSchema = new Schema({
+// //   name:String,
+// //   age: Number,
+// //   city:String,
+// //   gender: String
+// // })
+// // const User = mongoose.model("user",userSchema);
+
+// // const user1 = new User({name:"Rohit",age:20,city:"dwarka", gender:"Male"});
+// // await user1.save();
+// // await User.create({name:"manish", city:"patna", age:18});
+
+// // const ans = await User.find({});
+// // console.log(ans);
+
+// }
+
+// module.exports = main;
+// main()
+// .then(()=>console.log("connected to db"))
+// .catch((err)=>console.log(err))
